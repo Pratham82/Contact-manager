3a168b6c424e01fdfac73f8b1079bd95cb144d8d
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-03 19:28:08 +0530

    Added routes to the server

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+//Defining routes
+app.use("/api/users", require("./routes/users"))
+app.use("/api/contacts", require("./routes/contacts"))
+app.use("/api/auth", require("./routes/auth"))
+
 app.get("/", (req, res) =>
   res.json({ message: "Welcome to the Contact-manager API" })
 )
