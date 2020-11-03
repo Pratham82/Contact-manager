276016c047c1ccee7fcfd4786eac385d7cda6f23
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-03 20:06:46 +0530

    Added DB setup to server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express")
+const connectDB = require("./config/db")
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+// Connect DB
+connectDB()
 
 //Defining routes
 app.use("/api/users", require("./routes/users"))
