3a902135c3f024ce7fde978246af920e8eab0d6f
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-04 19:50:04 +0530

    Added other routes to server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require("express")
-const connectDB = require("./config/db")
-
+const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
 
 const PORT = process.env.PORT || 5000
@@ -8,13 +7,16 @@ const PORT = process.env.PORT || 5000
 // Connect DB
 connectDB()
 
-//Defining routes
-app.use("/api/users", require("./routes/users"))
-app.use("/api/contacts", require("./routes/contacts"))
-app.use("/api/auth", require("./routes/auth"))
+//Init middleware
+app.use(express.json({ extended: false }))
 
-app.get("/", (req, res) =>
-  res.json({ message: "Welcome to the Contact-manager API" })
+//Defining routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/auth', require('./routes/auth'))
+
+app.get('/', (req, res) =>
+  res.json({ message: 'Welcome to the Contact-manager API' })
 )
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
