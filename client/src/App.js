8d14cddeaa10689dfd07ab5e929927c49b5f04e1
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-09 19:01:00 +0530

    Added Context in App

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,22 @@ import Navbar from './components/layout/Navbar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import ContactState from './contexts/contact/ContactState'
 
 export default function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   )
 }
