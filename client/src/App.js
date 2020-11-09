36f6626a79f1668be6f9886cca0249ff0334a0f5
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-09 14:28:37 +0530

    Added cliet side React Code

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+import './App.css'
+import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+
+export default function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  )
+}
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">This is full stack App for managing contacts</p>
+      <p className="bg-dark p">
+        <strong>Varsion</strong>1.0.0
+      </p>
+    </div>
+  )
+}
+export default About
