14fc3bb0f6bb0646a71f381de878ca19ba1ff4fe
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-12 15:50:14 +0530

    Added Login and Register page component

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { name, email, password, password2 } = user
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value })
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log('Register submit', user)
+  }
+
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Register</span>{' '}
+      </h1>
+      <form action="" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Email</label>
+          <input type="text" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Password</label>
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Confirm Password</label>
+          <input
+            type="text"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  )
+}
+export default Register
