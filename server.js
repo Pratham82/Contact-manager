0f6e364b9004a4d7d2996518c080dae6c5f2c692
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-13 16:54:55 +0530

    Removed inital route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 const app = express()
 
 const PORT = process.env.PORT || 5000
@@ -15,8 +16,18 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use('/api/auth', require('./routes/auth'))
 
-app.get('/', (req, res) =>
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+}
+
+/*app.get('/', (req, res) =>
   res.json({ message: 'Welcome to the Contact-manager API' })
-)
+)*/
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
